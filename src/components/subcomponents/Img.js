2744b41e6d61bd387e6a173c66bbdb3b8c1e670e
Allison Bradley <allison.bradley@outlook.com>
2020-02-26 13:24:10 -0700

    made images work

--- src/components/subcomponents/Img.js
+++ src/components/subcomponents/Img.js
@@ -1,17 +1,12 @@
 import React from "react"
 // import { ImgWrapper } from "../../styles"
-import "../images/budget-buddy.png"
-import "../images/weather-app.png"
-import "../images/rps-go.png"
-import "../images/tabletop-tap.png"
-import "../images/employee-manager.png"
-import "../images/hamburger-helper.png"
 
 export const Img = (props) => {
+ 
     return (
             <>
             <a target="_blank" rel="noopener noreferrer" href={props.href}>
-            <img alt={props.alt} src=""/>
+            <img alt={props.alt} src={`${process.env.PUBLIC_URL}/images/${props.img}.png`}/>
             </a>
             </>
     )
