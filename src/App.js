5dcc6d08f79ff0162cad4cfd74286ca2ebc24a80
Allison Bradley <allison.bradley@outlook.com>
2021-01-02 13:44:31 -0700

    Programmed footer to change language

--- src/App.js
+++ src/App.js
@@ -14,18 +14,20 @@ function App() {
   })
 
   useEffect(() => {
-    setState({...state, language: "English"})
+    setState({ ...state, language: "English" })
     console.log("State changed")
   }, [])
 
   return (
-  <Router>
-   <Route exact path="/" component={About} />
-   <Route exact path="/about" component={About} />
-   <Route exact path="/portfolio" component={Portfolio} />
-   <Route exact path="/contact" component={Contact} />
-   <Footer />
-  </Router>
+    <Router>
+      <languageContext.Provider value={{ state, setState }}>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer />
+      </languageContext.Provider>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useContext } from "react"
+import { languageContext } from "../languageContext"
 import { FooterWrapper } from "../styles"
 import { Link } from "react-router-dom"
 import Japan from "./images/japan.png"
 import USA from "./images/usa.png"
 
 export const Footer = () => {
+
+const { state, setState } = useContext(languageContext)
+
+const handleClick = (event) => {
+    const {name} = event.target
+    setState({...state, language: name})
+    console.log(state)
+}
+
     return (
        <FooterWrapper>
             <div id="footer">
@@ -13,8 +23,8 @@ export const Footer = () => {
                 <a target="_blank" rel="noopener noreferrer" href={`${process.env.PUBLIC_URL}/images/060120.pdf`}><i className="far fa-clipboard fa-3x"></i></a>
                 <Link to="/" href="https://www.linkedin.com/in/allison-bradley-bba219198/"><i className="fas fa-home fa-3x"></i></Link>
                 <br/>
-                <button><img src={USA}/></button>
-                <button><img src={Japan}/></button>
+                <button name="USA" onClick={handleClick}><img name="USA" src={USA}/></button>
+                <button name="Japan" onClick={handleClick}><img name="Japan" src={Japan}/></button>
             </div>
         </FooterWrapper>
     )
