85f9de8224d5154d8aef160907145d959e9ccd5a
Allison Bradley <allison.bradley@outlook.com>
2021-01-11 17:20:34 -0700

    Updating resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ const handleClick = (event) => {
             <div id="footer">
                 <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/allison-bradley-bba219198/"><i className="fab fa-linkedin-in fa-3x"></i></a>
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/BestBroBradley"><i className="fab fa-github fa-3x"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href={`${process.env.PUBLIC_URL}/images/060120.pdf`}><i className="far fa-clipboard fa-3x"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href={`${process.env.PUBLIC_URL}/images/011121.pdf`}><i className="far fa-clipboard fa-3x"></i></a>
                 <Link to="/" href="https://www.linkedin.com/in/allison-bradley-bba219198/"><i className="fas fa-home fa-3x"></i></Link>
                 <br/>
                 <button name="USA" onClick={handleClick}><img name="USA" src={USA}/></button>
